7a6fc2af5364ade5618bb4ba8db84f76c4dd7f11
MelShellam <13166551+GadgetGeekUK@users.noreply.github.com>
2023-01-01 21:57:31 +0000

    New Theme, initial recipe and Layout.liquid

--- src/OrchardCore.Themes/Youoko/ResourceManifestOptionsConfiguration.cs
+++ src/OrchardCore.Themes/Youoko/ResourceManifestOptionsConfiguration.cs
@@ -12,9 +12,44 @@ namespace OrchardCore.Themes.Youoko
             _manifest = new ResourceManifest();
 
             _manifest
-                .DefineStyle("Youoko-bootstrap-oc")
-                .SetUrl("~/Youoko/css/bootstrap-oc.min.css", "~/Youoko/css/bootstrap-oc.css")
-                .SetVersion("0.0.1");
+                .DefineStyle("Youoko-Bootstrap")
+                .SetUrl("~/Youoko/css/bootstrap.min.css", "~/Youoko/css/bootstrap.min.css")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineStyle("Youoko-Bootstrap-Addons")
+                .SetUrl("~/Youoko/css/bootstrap.addons.css", "~/Youoko/css/bootstrap.addons.css")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineStyle("Youoko-Animations")
+                .SetUrl("~/Youoko/css/animations.css", "~/Youoko/css/animations.css")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineStyle("Youoko-Main")
+                .SetUrl("~/Youoko/css/main.css", "~/Youoko/css/main.css")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineScript("Youoko-Main")
+                .SetUrl("~/Youoko/js/main.js", "~/Youoko/js/main.js")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineScript("Youoko-Compressed")
+                .SetUrl("~/Youoko/js/compressed.js", "~/Youoko/js/compressed.js")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineScript("Youoko-BootstrapAddons")
+                .SetUrl("~/Youoko/js/bootstrap.addons.js", "~/Youoko/js/bootstrap.addons.js")
+                .SetVersion("1.0.0");
+
+            _manifest
+                .DefineScript("Youoko-CustomModernizer")
+                .SetUrl("~/Youoko/js/vendor/modernizer-custom.js", "~/Youoko/js/vendor/modernizer-custom.js")
+                .SetVersion("1.0.0");
         }
 
         public void Configure(ResourceManagementOptions options)
